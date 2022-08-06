3f5b8a3416111008812206bf9ac8f94be81d48ee
ShafaiJMI <imamshafai02@gmail.com>
2022-08-06 17:00:01 +0530

    Production build

---
+++ build/precache-manifest.4a27e47227d5b8bb0e842b1845eeacf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d20d7b840a18b81295c5",
+    "url": "/static/css/main.bab8c6de.chunk.css"
+  },
+  {
+    "revision": "d20d7b840a18b81295c5",
+    "url": "/static/js/main.d20d7b84.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "c69aa1e0cf2737a362f6",
+    "url": "/static/js/2.c69aa1e0.chunk.js"
+  },
+  {
+    "revision": "7ee0e2da555c3fd35366",
+    "url": "/static/js/runtime~main.7ee0e2da.js"
+  },
+  {
+    "revision": "6b929b645f35395b35940c54b6e0f2c0",
+    "url": "/index.html"
+  }
+];
